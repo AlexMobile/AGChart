b4fab04379e079ce0e1a5424e84a012daaad871a
Alexey Golovenkov <mobidevelop@yandex.ru>
2014-12-19 13:13:59 +0300

    Initial commit

---
+++ AGChart/AGChart/AGChart.h
@@ -0,0 +1,19 @@
+//
+//  AGChart.h
+//  AGChart
+//
+//  Created by Alexey Golovenkov on 19/12/14.
+//  Copyright (c) 2014 Alexey Golovenkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AGChart.
+FOUNDATION_EXPORT double AGChartVersionNumber;
+
+//! Project version string for AGChart.
+FOUNDATION_EXPORT const unsigned char AGChartVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AGChart/PublicHeader.h>
+
+
